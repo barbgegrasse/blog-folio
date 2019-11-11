780f46c1f88b998a4c8943fd209fd62fd44d9b62
kovsky <johan@developpeur-web.tech>
2019-11-11 21:10:24 +0100

    correction page remerciement

---
+++ src/pages/merci.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LayoutIndex from '../components/LayoutIndex'
+
+const Merci = () => (
+  <LayoutIndex>
+    <h1>Merci</h1>
+    <p>Votre demande de contact a été prise en compte.</p>
+  </LayoutIndex>
+)
+
+export default Merci
